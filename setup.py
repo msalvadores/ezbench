5e29960d2bee1824ed149a1e6003a7f82209bb21
msalvadores <msalvadores@gmail.com>
2013-08-08 17:45:40 -0700

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ezbench',
+    version='0.1',
+    description='ezbench - Easy Benchmark for Python',
+    author='Manuel Salvadores',
+    author_email='msalvadores@gmail.com',
+    url='https://github.com/msalvadores/ezbench',
+    packages=['ezbench', 'ezbench.report'],
+)
