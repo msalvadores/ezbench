8cd8e4c157fc4bc1e2d848becdd5712d1d989e4a
msalvadores <msalvadores@gmail.com>
2013-08-09 10:36:13 -0700

    benchmark measures by thread

---
+++ ezbench/bench_thread.py
@@ -0,0 +1,18 @@
+from __future__ import with_statement
+import pdb
+import threading
+
+class BenchmarkThread:
+    def __init__(self,name,benchmark):
+        self.name = name
+        self.measures = dict()
+        self.lock = threading.Lock()
+        self.benchmark = benchmark
+        self.id_measure = 0
+
+    def add_measure(self,init,end,data=None)
+        with self.lock:
+            self.id_measure += 1
+            measure = self.benchmark.create_measure(self.id_measure,
+                                          init, end, data=data)
+        self.measures.append(measure)
