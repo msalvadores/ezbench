0d59b8120f608df9a38bb8976df30d7da062a1b0
msalvadores <msalvadores@gmail.com>
2013-08-15 17:49:39 -0700

    control exception in wrapper function

--- ezbench/wrapper_function.py
+++ ezbench/wrapper_function.py
@@ -6,7 +6,11 @@ def ezbench_wrapper(func,benchmark,group,data=None,subgroups=None):
     def wrapped(*args, **kw):
         self = args[0]
         init = time.time()
-        wrapped_result = func(*args, **kw)
+        exception = None
+        try:
+            wrapped_result = func(*args, **kw)
+        except e:
+            exception = e
         end = time.time()
         thread = threading.currentThread()
         if not thread.__dict__.has_key('ezbench'):
@@ -19,5 +23,7 @@ def ezbench_wrapper(func,benchmark,group,data=None,subgroups=None):
             data_call = data(self)
 
         thread.ezbench.add_measure(group,init,end,data=data_call,subgroups=subgroup_data)
+        if exception:
+            raise exception
         return wrapped_result
     return wrapped
